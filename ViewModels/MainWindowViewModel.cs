a5340c1f0d0971455dd380a945058ec241388924
ubersk <kruchinin.s.s@gmail.com>
2022-10-10 15:40:36 +0300

    Add demon

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,7 +1,24 @@
-﻿namespace Kruchinin_28092022.ViewModels
+﻿using Kruchinin_28092022.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Kruchinin_28092022.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
-        public string Greeting => "Welcome to Avalonia!";
+        private List<ProductMaterial> productMaterials;
+        public MainWindowViewModel()
+        {
+            productMaterials = new VosmerkaContext().ProductMaterials
+                .Include(p => p.Product)
+                .ThenInclude(pt => pt.ProductType)
+                .ToList();
+        }
+        public List<ProductMaterial> ProductMaterials
+        {
+            get => productMaterials;
+            set => productMaterials = value;
+        }
     }
 }
---
+++ Вариант 3/Сессия 1/Clean identity.sql
@@ -0,0 +1,1 @@
+DBCC CHECKIDENT (Material, RESEED, 0)
