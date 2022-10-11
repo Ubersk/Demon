2f5e4ff043d161ffad60dbc864f4e15a62795ba0
ubersk <kruchinin.s.s@gmail.com>
2022-10-11 09:15:03 +0300

    Add Button

---
+++ Pages/PageAddProduct.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Kruchinin_28092022.Pages
+{
+    public partial class PageAddProduct : Window
+    {
+        public PageAddProduct()
+        {
+            InitializeComponent();
+        }
+    }
+}
